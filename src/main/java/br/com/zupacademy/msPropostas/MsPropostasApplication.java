1a2f4fd656141a1b0ee076a546468d2d920b8647
Breno Goya <breno.goya@zup.com.br>
2021-08-17 10:18:11 -0300

    Setup projeto

---
+++ src/main/java/br/com/zupacademy/msPropostas/MsPropostasApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.msPropostas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPropostasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPropostasApplication.class, args);
+	}
+
+}
